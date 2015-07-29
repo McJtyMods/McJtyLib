ca968c48da912324e9e537f58d65e9d578d024d0
McJty <mcjty1@gmail.com>
2015-07-29 09:14:21 +0200

    Fixed build.gradle and version 1.0.0

--- src/main/java/mcjty/base/ModBase.java
+++ src/main/java/mcjty/base/ModBase.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 
 public interface ModBase {
-    public static final int VERSION = 1;
+    public static final String VERSION = "1.0.0";
 
     /**
      * This is called whenever the user change the gui style.
