a2e72e49a4a54cd941eb4663636fcf5508afd68c
McJty <mcjty1@gmail.com>
2015-12-27 14:14:49 +0100

    Version 1.8.8-1.8.1beta1

--- src/main/java/mcjty/lib/McJtyLibVersion.java
+++ src/main/java/mcjty/lib/McJtyLibVersion.java
@@ -4,5 +4,5 @@ public class McJtyLibVersion {
     private McJtyLibVersion() {
     }
 
-    public static final String VERSION = "1.8.0beta1";
+    public static final String VERSION = "1.8.8-1.8.1beta1";
 }
