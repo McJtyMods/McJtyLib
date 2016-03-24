9d9e54725e0afee6c824ac2b02aee1e15b07611e
McJty <mcjty1@gmail.com>
2016-03-24 12:12:47 +0100

    1.9.0-1.8.1beta9

--- src/main/java/mcjty/lib/McJtyLibVersion.java
+++ src/main/java/mcjty/lib/McJtyLibVersion.java
@@ -4,5 +4,5 @@ public class McJtyLibVersion {
     private McJtyLibVersion() {
     }
 
-    public static final String VERSION = "1.8.9-1.8.1beta8";
+    public static final String VERSION = "1.9.0-1.8.1beta9";
 }
