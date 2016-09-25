66c9f5148cf82cc5136c46d2c0f37dba03e48860
McJty <mcjty1@gmail.com>
2016-09-25 17:05:34 +0200

    2.1.3

--- src/main/java/mcjty/lib/McJtyLib.java
+++ src/main/java/mcjty/lib/McJtyLib.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.relauncher.Side;
 @SuppressWarnings("unused")
 public class McJtyLib {
 
-    public static final String VERSION = "1.10-2.1.2";
+    public static final String VERSION = "1.10-2.1.3";
     public static final String OWNER = "McJty", PROVIDES = "McJtyLib";
 
     private static final ResourceLocation PREFERENCES_CAPABILITY_KEY;
