8558963f92a099df360bc19024f0d739a2e6ef07
McJty <mcjty1@gmail.com>
2016-10-18 10:02:14 +0200

    Added support for reading/write fluids to the network buffer

--- src/main/java/mcjty/lib/network/NetworkTools.java
+++ src/main/java/mcjty/lib/network/NetworkTools.java
@@ -5,11 +5,35 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fluids.FluidStack;
 
 import java.io.IOException;
 import java.util.Collection;
 
 public class NetworkTools {
+
+    public static FluidStack readFluidStack(ByteBuf dataIn) {
+        PacketBuffer buf = new PacketBuffer(dataIn);
+        try {
+            NBTTagCompound nbt = buf.readNBTTagCompoundFromBuffer();
+            return FluidStack.loadFluidStackFromNBT(nbt);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void writeFluidStack(ByteBuf dataOut, FluidStack fluidStack) {
+        PacketBuffer buf = new PacketBuffer(dataOut);
+        NBTTagCompound nbt = new NBTTagCompound();
+        fluidStack.writeToNBT(nbt);
+        try {
+            buf.writeNBTTagCompoundToBuffer(nbt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static NBTTagCompound readTag(ByteBuf dataIn) {
         PacketBuffer buf = new PacketBuffer(dataIn);
         try {
