80aa77df78101f2b6125baf9955f37baaceb6a52
McJty <mcjty1@gmail.com>
2017-02-20 06:05:09 +0100

    Fix on 1.10 for item tooltips

--- src/main/java/mcjty/lib/container/GenericItemBlock.java
+++ src/main/java/mcjty/lib/container/GenericItemBlock.java
@@ -2,6 +2,10 @@ package mcjty.lib.container;
 
 import mcjty.lib.compat.CompatItemBlock;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 public class GenericItemBlock extends CompatItemBlock {
     private final GenericBlock genericBlock;
@@ -10,4 +14,12 @@ public class GenericItemBlock extends CompatItemBlock {
         super(block);
         genericBlock = (GenericBlock) block;
     }
+
+
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List<String> list, boolean advancedToolTip) {
+//        super.addInformation(itemStack, player, list, advancedToolTip);
+        genericBlock.addInformation(itemStack, player, list, advancedToolTip);
+    }
+
 }
