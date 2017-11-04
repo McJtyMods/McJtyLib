5a82f0d45f13fded16ef646cd7f01f4250b9980e
McJty <mcjty1@gmail.com>
2017-11-04 05:45:37 +0100

    Merged

--- src/main/java/mcjty/lib/McJtyLib.java
+++ src/main/java/mcjty/lib/McJtyLib.java
@@ -29,7 +29,6 @@ import net.minecraftforge.fml.relauncher.Side;
 public class McJtyLib {
 
     public static final String VERSION = "2.4.4";
-    public static final String VERSION = "2.4.3";
     public static final String OWNER = "McJty", PROVIDES = "mcjtylib_ng";
 
     private static final ResourceLocation PREFERENCES_CAPABILITY_KEY;
