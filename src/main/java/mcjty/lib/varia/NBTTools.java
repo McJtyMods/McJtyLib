d04ec31dfb516595098887b268efec0896926386
McJty <mcjty1@gmail.com>
2017-12-18 16:18:21 +0100

    Added NBTTools

---
+++ src/main/java/mcjty/lib/varia/NBTTools.java
@@ -0,0 +1,54 @@
+package mcjty.lib.varia;
+
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import org.apache.commons.lang3.StringUtils;
+
+public class NBTTools {
+
+    public static StringBuffer appendIndent(StringBuffer buffer, int indent) {
+        return buffer.append(StringUtils.repeat(' ', indent));
+    }
+
+    public static void convertNBTtoJson(StringBuffer buffer, NBTTagList tagList, int indent) {
+        for (int i = 0 ; i < tagList.tagCount() ; i++) {
+            NBTTagCompound compound = tagList.getCompoundTagAt(i);
+            appendIndent(buffer, indent).append("{\n");
+            convertNBTtoJson(buffer, compound, indent + 4);
+            appendIndent(buffer, indent).append("},\n");
+        }
+    }
+
+    public static void convertNBTtoJson(StringBuffer buffer, NBTTagCompound tagCompound, int indent) {
+        boolean first = true;
+        for (Object o : tagCompound.getKeySet()) {
+            if (!first) {
+                buffer.append(",\n");
+            }
+            first = false;
+
+            String key = (String) o;
+            NBTBase tag = tagCompound.getTag(key);
+            appendIndent(buffer, indent).append(key).append(':');
+            if (tag instanceof NBTTagCompound) {
+                NBTTagCompound compound = (NBTTagCompound) tag;
+                buffer.append("{\n");
+                convertNBTtoJson(buffer, compound, indent + 4);
+                appendIndent(buffer, indent).append('}');
+            } else if (tag instanceof NBTTagList) {
+                NBTTagList list = (NBTTagList) tag;
+                buffer.append("[\n");
+                convertNBTtoJson(buffer, list, indent + 4);
+                appendIndent(buffer, indent).append(']');
+            } else {
+                buffer.append(tag);
+            }
+        }
+        if (!first) {
+            buffer.append("\n");
+        }
+    }
+
+
+}
