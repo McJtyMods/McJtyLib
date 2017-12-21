18b939ac7824e60574420742f139799b10f56674
McJty <mcjty1@gmail.com>
2017-12-21 11:12:50 +0100

    Some small BlockPosTools tweaks

--- src/main/java/mcjty/lib/varia/BlockPosTools.java
+++ src/main/java/mcjty/lib/varia/BlockPosTools.java
@@ -4,7 +4,15 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 
 public class BlockPosTools {
+
     public static final BlockPos INVALID = new BlockPos(-1, -1, -1);
+
+    public static boolean isAdjacent(BlockPos c1, BlockPos c2) {
+        int dx = Math.abs(c2.getX() - c1.getX())+1;
+        int dy = Math.abs(c2.getY() - c1.getY())+1;
+        int dz = Math.abs(c2.getZ() - c1.getZ())+1;
+        return dx * dy * dz == 2;
+    }
 
     public static int area(BlockPos c1, BlockPos c2) {
         return (c2.getX()-c1.getX()+1) * (c2.getY()-c1.getY()+1) * (c2.getZ()-c1.getZ()+1);
