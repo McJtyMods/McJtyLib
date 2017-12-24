d07f8d31b531e6ba00f201384dea942988028f33
McJty <mcjty1@gmail.com>
2017-12-24 06:26:05 +0100

    2.5.4

--- src/main/java/mcjty/lib/McJtyLib.java
+++ src/main/java/mcjty/lib/McJtyLib.java
@@ -34,7 +34,7 @@ import java.util.Map;
 @SuppressWarnings("unused")
 public class McJtyLib {
 
-    public static final String VERSION = "2.5.3";
+    public static final String VERSION = "2.5.4";
     public static final String OWNER = "McJty", PROVIDES = "mcjtylib_ng";
 
     private static final ResourceLocation PREFERENCES_CAPABILITY_KEY;
