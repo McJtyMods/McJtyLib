37aca335b782de3de346cab2c9e6278e1076b028
McJty <mcjty1@gmail.com>
2018-05-14 10:05:04 +0200

    Removed the obsolete Info packet system from McJtyLib. Rewrote the inventory info packet from RFTools with a custom packet

--- src/main/java/mcjty/lib/network/PacketHandler.java
+++ src/main/java/mcjty/lib/network/PacketHandler.java
@@ -1,9 +1,5 @@
 package mcjty.lib.network;
 
-import mcjty.lib.network.clientinfo.InfoPacketClient;
-import mcjty.lib.network.clientinfo.InfoPacketServer;
-import mcjty.lib.network.clientinfo.PacketGetInfoFromServer;
-import mcjty.lib.network.clientinfo.PacketReturnInfoToClient;
 import mcjty.lib.typed.TypedMap;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
@@ -21,38 +17,10 @@ public class PacketHandler {
     // For the client-info packet system:
     private static int packetId = INTERNAL_PACKETS;
 
-    private static Map<Integer,Class<? extends InfoPacketClient>> clientInfoPackets = new HashMap<>();
-    private static Map<Integer,Class<? extends InfoPacketServer>> serverInfoPackets = new HashMap<>();
-    private static Map<Class<? extends InfoPacketClient>,Integer> clientInfoPacketsToId = new HashMap<>();
-    private static Map<Class<? extends InfoPacketServer>,Integer> serverInfoPacketsToId = new HashMap<>();
-
     public static Map<String,SimpleNetworkWrapper> modNetworking = new HashMap<>();
 
     public static int nextPacketID() {
         return packetId++;
-    }
-
-    public static void register(Integer id, Class<? extends InfoPacketServer> serverClass, Class<? extends InfoPacketClient> clientClass) {
-        serverInfoPackets.put(id, serverClass);
-        clientInfoPackets.put(id, clientClass);
-        serverInfoPacketsToId.put(serverClass, id);
-        clientInfoPacketsToId.put(clientClass, id);
-    }
-
-    public static Class<? extends InfoPacketServer> getServerInfoPacket(int id) {
-        return serverInfoPackets.get(id);
-    }
-
-    public static Integer getServerInfoPacketId(Class<? extends InfoPacketServer> clazz) {
-        return serverInfoPacketsToId.get(clazz);
-    }
-
-    public static Class<? extends InfoPacketClient> getClientInfoPacket(int id) {
-        return clientInfoPackets.get(id);
-    }
-
-    public static Integer getClientInfoPacketId(Class<? extends InfoPacketClient> clazz) {
-        return clientInfoPacketsToId.get(clazz);
     }
 
     @Deprecated
@@ -80,14 +48,12 @@ public class PacketHandler {
         networkWrapper.registerMessage(PacketRequestDataFromServer.Handler.class, PacketRequestDataFromServer.class, startIndex++, Side.SERVER);
 //        networkWrapper.registerMessage(PacketUpdateNBTItem.Handler.class, PacketUpdateNBTItem.class, startIndex++, Side.SERVER);
 //        networkWrapper.registerMessage(PacketUpdateNBTItemInventory.Handler.class, PacketUpdateNBTItemInventory.class, startIndex++, Side.SERVER);
-        networkWrapper.registerMessage(PacketGetInfoFromServer.Handler.class, PacketGetInfoFromServer.class, nextID(), Side.SERVER);
         networkWrapper.registerMessage(PacketDumpItemInfo.Handler.class, PacketDumpItemInfo.class, startIndex++, Side.SERVER);
         networkWrapper.registerMessage(PacketDumpBlockInfo.Handler.class, PacketDumpBlockInfo.class, startIndex++, Side.SERVER);
 
         // Client side
         networkWrapper.registerMessage(PacketSendClientCommandHandler.class, PacketSendClientCommand.class, startIndex++, Side.CLIENT);
         networkWrapper.registerMessage(PacketDataFromServer.Handler.class, PacketDataFromServer.class, startIndex++, Side.CLIENT);
-        networkWrapper.registerMessage(PacketReturnInfoToClient.Handler.class, PacketReturnInfoToClient.class, nextID(), Side.CLIENT);
         networkWrapper.registerMessage(PacketSendGuiData.Handler.class, PacketSendGuiData.class, startIndex++, Side.CLIENT);
 
         return startIndex;
