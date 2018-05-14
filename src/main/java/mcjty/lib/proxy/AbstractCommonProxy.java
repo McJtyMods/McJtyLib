707d3fa86380c82afebe67c3a6da47be0bea0139
McJty <mcjty1@gmail.com>
2018-05-14 11:09:07 +0200

    Rationalized the proxies. There is now an AbstractCommonProxy in McJtyLib

---
+++ src/main/java/mcjty/lib/proxy/AbstractCommonProxy.java
@@ -0,0 +1,51 @@
+package mcjty.lib.proxy;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import mcjty.lib.McJtyLib;
+import mcjty.lib.base.GeneralConfig;
+import mcjty.lib.varia.WrenchChecker;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+public abstract class AbstractCommonProxy {
+
+    public File modConfigDir;
+    protected Configuration mainConfig = null;
+
+    public void preInit(FMLPreInitializationEvent e) {
+        McJtyLib.preInit(e);
+        GeneralConfig.preInit(e);
+        modConfigDir = e.getModConfigurationDirectory();
+    }
+
+    public void init(FMLInitializationEvent e) {
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+        WrenchChecker.init();
+    }
+
+    public World getClientWorld() {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+    public EntityPlayer getClientPlayer() {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+    public <V> ListenableFuture<V> addScheduledTaskClient(Callable<V> callableToSchedule) {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+    public ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule) {
+        throw new IllegalStateException("This should only be called from client side");
+    }
+
+}
