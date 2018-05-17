dae7a84c7b15e7b51164f9a1f1ce819ec9405157
McJty <mcjty1@gmail.com>
2018-05-17 20:04:48 +0200

    Work on per-biome and inside/outside based poison settings

--- src/main/java/mcjty/lib/varia/WorldTools.java
+++ src/main/java/mcjty/lib/varia/WorldTools.java
@@ -1,15 +1,35 @@
 package mcjty.lib.varia;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 
 public class WorldTools {
+
     public static boolean chunkLoaded(World world, BlockPos pos) {
         if (world == null || pos == null) {
             return false;
         }
         return world.isBlockLoaded(pos);
 //        return world.getChunkProvider().getLoadedChunk(pos.getX() >> 4, pos.getZ() >> 4) != null && world.getChunkFromBlockCoords(pos).isLoaded();
+    }
+
+    /**
+     * Find a biome based on ID or registry name
+     */
+    public static Biome findBiome(String biomeId) {
+        Biome biome = Biome.REGISTRY.getObject(new ResourceLocation(biomeId));
+        if (biome == null) {
+            for (Biome b : Biome.REGISTRY) {
+                ResourceLocation registryName = b.getRegistryName();
+                if (registryName != null && biomeId.equals(registryName.getResourcePath())) {
+                    biome = b;
+                    break;
+                }
+            }
+        }
+        return biome;
     }
 
 
