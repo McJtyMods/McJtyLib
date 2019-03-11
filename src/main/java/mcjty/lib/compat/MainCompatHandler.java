1b381d5d604d4680257cb76b33f1d145aa17d812
McJty <mcjty1@gmail.com>
2019-03-11 20:06:13 +0100

    Fixed a problem with TOP and WAILA handlers being duplicated

--- src/main/java/mcjty/lib/compat/MainCompatHandler.java
+++ src/main/java/mcjty/lib/compat/MainCompatHandler.java
@@ -4,9 +4,6 @@ import mcjty.lib.compat.theoneprobe.TOPCompatibility;
 import net.minecraftforge.fml.common.Loader;
 import mcjty.lib.compat.waila.WailaCompatibility;
 
-/**
- * Created by Elec332 on 17-10-2015.
- */
 public class MainCompatHandler {
 
     public static void registerWaila() {
