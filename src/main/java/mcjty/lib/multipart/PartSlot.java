4ae17266470c71219de282ec34a81d99af3221ad
McJty <mcjty1@gmail.com>
2019-04-13 07:14:51 +0200

    Round robin works

--- src/main/java/mcjty/lib/multipart/PartSlot.java
+++ src/main/java/mcjty/lib/multipart/PartSlot.java
@@ -86,4 +86,59 @@ public enum PartSlot {
     public static PartSlot byName(String name) {
         return SLOT_MAP.get(name);
     }
+
+    public PartSlot getBackSlot() {
+        switch (this) {
+            case DOWN_NW:
+                return DOWN_NW_BACK;
+            case DOWN_NE:
+                return DOWN_NE_BACK;
+            case DOWN_SW:
+                return DOWN_SW_BACK;
+            case DOWN_SE:
+                return DOWN_SE_BACK;
+            case UP_NW:
+                return UP_NW_BACK;
+            case UP_NE:
+                return UP_NE_BACK;
+            case UP_SW:
+                return UP_SW_BACK;
+            case UP_SE:
+                return UP_SE_BACK;
+            case NORTH_UW:
+                return NORTH_UW_BACK;
+            case NORTH_UE:
+                return NORTH_UE_BACK;
+            case NORTH_DW:
+                return NORTH_DW_BACK;
+            case NORTH_DE:
+                return NORTH_DE_BACK;
+            case SOUTH_UW:
+                return SOUTH_UW_BACK;
+            case SOUTH_UE:
+                return SOUTH_UE_BACK;
+            case SOUTH_DW:
+                return SOUTH_DW_BACK;
+            case SOUTH_DE:
+                return SOUTH_DE_BACK;
+            case WEST_US:
+                return WEST_US_BACK;
+            case WEST_UN:
+                return WEST_UN_BACK;
+            case WEST_DS:
+                return WEST_DS_BACK;
+            case WEST_DN:
+                return WEST_DN_BACK;
+            case EAST_US:
+                return EAST_US_BACK;
+            case EAST_UN:
+                return EAST_UN_BACK;
+            case EAST_DS:
+                return EAST_DS_BACK;
+            case EAST_DN:
+                return EAST_DN_BACK;
+            default:
+                return this;
+        }
+    }
 }
