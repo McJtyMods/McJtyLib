2b2d2324a7465769438512053ecfe7dc6ce4e81b
McJty <slto15aGH10>
2019-12-05 15:47:00 +0100

    Removed some debugging info

--- src/main/java/mcjty/lib/network/PacketHandler.java
+++ src/main/java/mcjty/lib/network/PacketHandler.java
@@ -1,14 +1,9 @@
 package mcjty.lib.network;
 
 import mcjty.lib.typed.TypedMap;
-import net.minecraft.network.PacketBuffer;
-import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 import javax.annotation.Nonnull;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class PacketHandler {
 
@@ -34,8 +29,10 @@ public class PacketHandler {
 //        channel.registerMessage(startIndex++, PacketRequestDataFromServer.class, PacketRequestDataFromServer::toBytes, PacketRequestDataFromServer::new, PacketRequestDataFromServer::handle);
 //        channel.registerMessage(startIndex++, PacketDumpItemInfo.class, PacketDumpItemInfo::toBytes, PacketDumpItemInfo::new, PacketDumpItemInfo::handle);
 //        channel.registerMessage(startIndex++, PacketDumpBlockInfo.class, PacketDumpBlockInfo::toBytes, PacketDumpBlockInfo::new, PacketDumpBlockInfo::handle);
-        debugRegister("mcjtylib", channel, startIndex++, PacketSendPreferencesToClient.class, PacketSendPreferencesToClient::toBytes, PacketSendPreferencesToClient::new, PacketSendPreferencesToClient::handle);
-        debugRegister("mcjtylib", channel, startIndex++, PacketSetGuiStyle.class, PacketSetGuiStyle::toBytes, PacketSetGuiStyle::new, PacketSetGuiStyle::handle);
+        int index1 = startIndex++;
+        channel.registerMessage(index1, PacketSendPreferencesToClient.class, PacketSendPreferencesToClient::toBytes, PacketSendPreferencesToClient::new, PacketSendPreferencesToClient::handle);
+        int index = startIndex++;
+        channel.registerMessage(index, PacketSetGuiStyle.class, PacketSetGuiStyle::toBytes, PacketSetGuiStyle::new, PacketSetGuiStyle::handle);
 
         // Client side
 //        debugRegister(channel, );(startIndex++, PacketSendClientCommand.class, PacketSendClientCommand::toBytes, PacketSendClientCommand::new, PacketSendClientCommand::handle);
@@ -44,18 +41,25 @@ public class PacketHandler {
 //        debugRegister(channel, );(startIndex++, PacketFinalizeLogin.class, PacketFinalizeLogin::toBytes, PacketFinalizeLogin::new, PacketFinalizeLogin::handle);
     }
 
-    public static void registerStandardMessages(String context, int id, SimpleChannel channel) {
+    public static void registerStandardMessages(int id, SimpleChannel channel) {
 
         // Server side
-        debugRegister(context, channel, id++, PacketServerCommandTyped.class, PacketServerCommandTyped::toBytes, PacketServerCommandTyped::new, PacketServerCommandTyped::handle);
-        debugRegister(context, channel, id++, PacketSendServerCommand.class, PacketSendServerCommand::toBytes, PacketSendServerCommand::new, PacketSendServerCommand::handle);
-        debugRegister(context, channel, id++, PacketDumpItemInfo.class, PacketDumpItemInfo::toBytes, PacketDumpItemInfo::new, PacketDumpItemInfo::handle);
-        debugRegister(context, channel, id++, PacketDumpBlockInfo.class, PacketDumpBlockInfo::toBytes, PacketDumpBlockInfo::new, PacketDumpBlockInfo::handle);
+        int index6 = id++;
+        channel.registerMessage(index6, PacketServerCommandTyped.class, PacketServerCommandTyped::toBytes, PacketServerCommandTyped::new, PacketServerCommandTyped::handle);
+        int index5 = id++;
+        channel.registerMessage(index5, PacketSendServerCommand.class, PacketSendServerCommand::toBytes, PacketSendServerCommand::new, PacketSendServerCommand::handle);
+        int index4 = id++;
+        channel.registerMessage(index4, PacketDumpItemInfo.class, PacketDumpItemInfo::toBytes, PacketDumpItemInfo::new, PacketDumpItemInfo::handle);
+        int index3 = id++;
+        channel.registerMessage(index3, PacketDumpBlockInfo.class, PacketDumpBlockInfo::toBytes, PacketDumpBlockInfo::new, PacketDumpBlockInfo::handle);
 
         // Client side
-        debugRegister(context, channel, id++, PacketSendClientCommand.class, PacketSendClientCommand::toBytes, PacketSendClientCommand::new, PacketSendClientCommand::handle);
-        debugRegister(context, channel, id++, PacketDataFromServer.class, PacketDataFromServer::toBytes, PacketDataFromServer::new, PacketDataFromServer::handle);
-        debugRegister(context, channel, id++, PacketFinalizeLogin.class, PacketFinalizeLogin::toBytes, PacketFinalizeLogin::new, PacketFinalizeLogin::handle);
+        int index2 = id++;
+        channel.registerMessage(index2, PacketSendClientCommand.class, PacketSendClientCommand::toBytes, PacketSendClientCommand::new, PacketSendClientCommand::handle);
+        int index1 = id++;
+        channel.registerMessage(index1, PacketDataFromServer.class, PacketDataFromServer::toBytes, PacketDataFromServer::new, PacketDataFromServer::handle);
+        int index = id++;
+        channel.registerMessage(index, PacketFinalizeLogin.class, PacketFinalizeLogin::toBytes, PacketFinalizeLogin::new, PacketFinalizeLogin::handle);
     }
 
     // From client side only: send server command
@@ -63,8 +67,4 @@ public class PacketHandler {
         network.sendToServer(new PacketSendServerCommand(modid, command, arguments));
     }
 
-    public static <MSG> void debugRegister(String context, SimpleChannel channel, int index, Class<MSG> messageType, BiConsumer<MSG, PacketBuffer> encoder, Function<PacketBuffer, MSG> decoder, BiConsumer<MSG, Supplier<NetworkEvent.Context>> messageConsumer) {
-        System.out.println("###" + context + "###, class = " + messageType.getName() + ", id = " + index);
-        channel.registerMessage(index, messageType, encoder, decoder, messageConsumer);
-    }
 }
