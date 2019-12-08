bb5a942ca1a786d11868a1cde14e1209c5cd4ce9
McJty <mcjty1@gmail.com>
2019-12-08 20:44:55 +0100

    Cleaner code to permutate over all properties

--- src/main/java/mcjty/lib/varia/Tools.java
+++ src/main/java/mcjty/lib/varia/Tools.java
@@ -1,5 +1,7 @@
 package mcjty.lib.varia;
 
+import net.minecraft.state.Property;
+
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -26,5 +28,27 @@ public class Tools {
         } catch (ClassCastException e) {
             throw new IllegalArgumentException(error, e);
         }
+    }
+
+    private static <T extends Comparable<T>> String getValueName(Property<T> property, Object value) {
+        return property.getName((T)value);
+    }
+
+    private static void permutateProperties(Consumer<String> consumer, String left, Property<?>... properties) {
+        if (properties.length > 0) {
+            Property<?>[] tail = new Property<?>[properties.length-1];
+            System.arraycopy(properties, 1, tail, 0, properties.length - 1);
+            String name = properties[0].getName();
+            for (Comparable<?> value : properties[0].getAllowedValues()) {
+                permutateProperties(consumer, (left.isEmpty() ? "" : (left + ",")) + name + "=" + getValueName(properties[0], value), tail);
+            }
+        } else {
+            consumer.accept(left);
+        }
+    }
+
+    // Permutate over all properties and construct a property string that is given to the consumer
+    public static void permutateProperties(Consumer<String> consumer, Property<?>... properties) {
+        permutateProperties(consumer, "", properties);
     }
 }
