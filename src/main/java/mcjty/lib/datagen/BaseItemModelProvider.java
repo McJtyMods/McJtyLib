f42f4dbc96a22c1c43e0deba950ab856d3293a17
McJty <slto15aGH10>
2019-12-11 12:27:02 +0100

    Data generation changes and jsons

--- src/main/java/mcjty/lib/datagen/BaseItemModelProvider.java
+++ src/main/java/mcjty/lib/datagen/BaseItemModelProvider.java
@@ -2,6 +2,7 @@ package mcjty.lib.datagen;
 
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.Item;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -15,6 +16,15 @@ public abstract class BaseItemModelProvider extends ItemModelProvider {
     public void parentedBlock(Block block, String model) {
         getBuilder(block.getRegistryName().getPath())
                 .parent(new ModelFile.UncheckedModelFile(modLoc(model)));
+    }
 
+    public void itemGenerated(Item item, String texture) {
+        getBuilder(item.getRegistryName().getPath()).parent(getExistingFile(mcLoc("item/generated")))
+                .texture("layer0", texture);
+    }
+
+    public void itemHandheld(Item item, String texture) {
+        getBuilder(item.getRegistryName().getPath()).parent(getExistingFile(mcLoc("item/handheld")))
+                .texture("layer0", texture);
     }
 }
