e78d6adafa66f35460d36461f6bbaf7de37173ea
McJty <slto15aGH10>
2020-02-12 19:43:50 +0100

    Work on blockstate data generators. WIP

---
+++ src/main/java/mcjty/lib/blocks/BlockStateItem.java
@@ -0,0 +1,29 @@
+package mcjty.lib.blocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.BlockItemUseContext;
+
+import javax.annotation.Nullable;
+
+public class BlockStateItem extends BlockItem {
+
+    private final BlockState state;
+
+    public BlockStateItem(BlockState state, Properties builder) {
+        super(state.getBlock(), builder);
+        this.state = state;
+    }
+
+    @Nullable
+    @Override
+    protected BlockState getStateForPlacement(BlockItemUseContext context) {
+        BlockState stateForPlacement = super.getStateForPlacement(context);
+        if (stateForPlacement != null) {
+            return state;
+        }
+        return null;
+    }
+
+
+}
