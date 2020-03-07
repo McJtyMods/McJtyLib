4a128ff7f441195353497b0e683c2ebc2c808906
McJty <slto15aGH10>
2020-03-07 06:24:50 +0100

    More work on ariente and ariente world

--- src/main/java/mcjty/lib/varia/BlockTools.java
+++ src/main/java/mcjty/lib/varia/BlockTools.java
@@ -1,5 +1,6 @@
 package mcjty.lib.varia;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.item.ItemEntity;
@@ -9,6 +10,9 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.state.IProperty;
+import net.minecraft.state.IStateHolder;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
@@ -17,8 +21,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Random;
 
 public class BlockTools {
@@ -91,6 +99,68 @@ public class BlockTools {
             player.setPosition(pos.getX()+.5, pos.getY()+1.5, pos.getZ()+.5);
             blockStack.getItem().onItemUse(context);
             return world.getBlockState(pos);
+        }
+    }
+
+    // Write a blockstate to a string
+    public static String writeBlockState(BlockState tag) {
+        StringBuilder builder = new StringBuilder(ForgeRegistries.BLOCKS.getKey(tag.getBlock()).toString());
+        ImmutableMap<IProperty<?>, Comparable<?>> properties = tag.getValues();
+        if (!properties.isEmpty()) {
+            char c = '@';
+
+            for(Map.Entry<IProperty<?>, Comparable<?>> entry : properties.entrySet()) {
+                IProperty<?> property = entry.getKey();
+                builder.append(c); c = ',';
+                builder.append(property.getName());
+                builder.append('=');
+                builder.append(getName(property, entry.getValue()));
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private static <T extends Comparable<T>> String getName(IProperty<T> property, Comparable<?> cmp) {
+        return property.getName((T)cmp);
+    }
+
+    public static BlockState readBlockState(String s) {
+        String blockName;
+        String properties;
+        if (s.contains("@")) {
+            String[] split = StringUtils.split(s, '@');
+            blockName = split[0];
+            properties = split[1];
+        } else {
+            blockName = s;
+            properties = null;
+        }
+        Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName));
+        if (block == null) {
+            throw new RuntimeException("Cannot find block '" + blockName + "'!");
+        }
+        BlockState state = block.getDefaultState();
+        if (properties != null) {
+            StateContainer<Block, BlockState> statecontainer = state.getBlock().getStateContainer();
+            String[] split = StringUtils.split(properties, ',');
+            for (String pv : split) {
+                String[] sp = StringUtils.split(pv, '=');
+                IProperty<?> property = statecontainer.getProperty(sp[0]);
+                if (property != null) {
+                    state = setValueHelper(state, property, sp[1]);
+                }
+            }
+        }
+        return state;
+    }
+
+    private static <S extends IStateHolder<S>, T extends Comparable<T>> S setValueHelper(S state, IProperty<T> property, String value) {
+        Optional<T> optional = property.parseValue(value);
+        if (optional.isPresent()) {
+            return state.with(property, (T)(optional.get()));
+        } else {
+            return state;
         }
     }
 
