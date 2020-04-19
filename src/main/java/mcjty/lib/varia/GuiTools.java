e94147515b4cddd334505392e2a19d0de34548f5
McJty <mcjty1@gmail.com>
2020-04-19 07:58:03 +0200

    Work on the tablet functionality. WIP

--- src/main/java/mcjty/lib/varia/GuiTools.java
+++ src/main/java/mcjty/lib/varia/GuiTools.java
@@ -13,12 +13,14 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.function.Function;
 
 public class GuiTools {
 
@@ -49,33 +51,41 @@ public class GuiTools {
     }
 
     public static boolean openRemoteGui(@Nonnull PlayerEntity player, @Nullable DimensionType dimensionType, @Nonnull BlockPos pos) {
+        return openRemoteGui(player, dimensionType, pos,
+                te -> new INamedContainerProvider() {
+                    @Override
+                    public ITextComponent getDisplayName() {
+                        return new StringTextComponent("Remote Gui");
+                    }
+
+                    @Nullable
+                    @Override
+                    public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+                        return te.getCapability(CapabilityContainerProvider.CONTAINER_PROVIDER_CAPABILITY)
+                                .map(h -> h.createMenu(id, inventory, player))
+                                .orElse(null);
+                    }
+                });
+    }
+
+    public static boolean openRemoteGui(@Nonnull PlayerEntity player, @Nullable DimensionType dimensionType, @Nonnull BlockPos pos,
+                                        Function<TileEntity, INamedContainerProvider> provider) {
         if (dimensionType == null) {
             dimensionType = player.getEntityWorld().getDimension().getType();
         }
         World world = WorldTools.getWorld(player.getEntityWorld(), dimensionType);
         if (!WorldTools.isLoaded(world, pos)) {
-            player.sendStatusMessage(new StringTextComponent("Position is not loaded!"), false);
+            player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "Position is not loaded!"), false);
+            return false;
+        }
+        TileEntity te = world.getTileEntity(pos);
+        if (te == null) {
+            player.sendStatusMessage(new StringTextComponent(TextFormatting.RED + "Tile entity is missing!"), false);
             return false;
         }
 
-        NetworkHooks.openGui((ServerPlayerEntity) player, new INamedContainerProvider() {
-            @Override
-            public ITextComponent getDisplayName() {
-                return new StringTextComponent("Remote Gui");
-            }
-
-            @Nullable
-            @Override
-            public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
-                TileEntity te = world.getTileEntity(pos);
-                if (te != null) {
-                    return te.getCapability(CapabilityContainerProvider.CONTAINER_PROVIDER_CAPABILITY)
-                            .map(h -> h.createMenu(id, inventory, player))
-                            .orElse(null);
-                }
-                return null;
-            }
-        }, pos);
+        NetworkHooks.openGui((ServerPlayerEntity) player, provider.apply(te), pos);
         return true;
     }
+
 }
