c70eaacd43ac611fce5e7b3f37efebc841c1eda7
McJty <mcjty1@gmail.com>
2020-08-20 10:43:52 +0200

    Temporarily disabled Patchouli

--- src/main/java/mcjty/lib/compat/patchouli/PatchouliCompatibility.java
+++ src/main/java/mcjty/lib/compat/patchouli/PatchouliCompatibility.java
@@ -6,23 +6,24 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Util;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
-import vazkii.patchouli.api.PatchouliAPI;
+//import vazkii.patchouli.api.PatchouliAPI;
 
 public class PatchouliCompatibility {
 
     public static void openBookGUI(ServerPlayerEntity player, ResourceLocation id) {
-        if (ModSetup.patchouli) {
-            PatchouliAPI.instance.openBookGUI(player, id);
-        } else {
-            player.sendMessage(new StringTextComponent(TextFormatting.RED + "Patchouli is missing! No manual present"), Util.DUMMY_UUID);
-        }
+        // @todo 1.16
+//        if (ModSetup.patchouli) {
+//            PatchouliAPI.instance.openBookGUI(player, id);
+//        } else {
+//            player.sendMessage(new StringTextComponent(TextFormatting.RED + "Patchouli is missing! No manual present"), Util.DUMMY_UUID);
+//        }
     }
 
     public static void openBookEntry(ServerPlayerEntity player, ResourceLocation id, ResourceLocation entry, int page) {
-        if (ModSetup.patchouli) {
-            PatchouliAPI.instance.openBookEntry(player, id, entry, page);
-        } else {
-            player.sendMessage(new StringTextComponent(TextFormatting.RED + "Patchouli is missing! No manual present"), Util.DUMMY_UUID);
-        }
+//        if (ModSetup.patchouli) {
+//            PatchouliAPI.instance.openBookEntry(player, id, entry, page);
+//        } else {
+//            player.sendMessage(new StringTextComponent(TextFormatting.RED + "Patchouli is missing! No manual present"), Util.DUMMY_UUID);
+//        }
     }
 }
