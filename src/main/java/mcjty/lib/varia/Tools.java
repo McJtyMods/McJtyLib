21e879b16a242e32ade69cb8f4aeb73235685268
McJty <mcjty1@gmail.com>
2021-03-12 05:49:27 +0100

    If the mod is not minecraft then a dimlet tooltip will also show the mod for block and fluid dimlets

--- src/main/java/mcjty/lib/varia/Tools.java
+++ src/main/java/mcjty/lib/varia/Tools.java
@@ -1,9 +1,22 @@
 package mcjty.lib.varia;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Tools {
+
+    public static String getModName(IForgeRegistryEntry<?> entry) {
+        ResourceLocation registryName = entry.getRegistryName();
+        String modId = registryName == null ? "minecraft" : registryName.getNamespace();
+        return ModList.get().getModContainerById(modId)
+                .map(mod -> mod.getModInfo().getDisplayName())
+                .orElse(StringUtils.capitalize(modId));
+    }
 
     public static <INPUT extends BASE, BASE> void safeConsume(BASE o, Consumer<INPUT> consumer, String error) {
         try {
