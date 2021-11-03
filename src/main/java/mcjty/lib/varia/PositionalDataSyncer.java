8c4c00fbb00b64b2ede724446c5fabc50505377c
McJty <mcjty1@gmail.com>
2021-11-03 06:46:56 +0100

    New system to sync data. WIP

---
+++ src/main/java/mcjty/lib/varia/PositionalDataSyncer.java
@@ -0,0 +1,11 @@
+package mcjty.lib.varia;
+
+/**
+ * Use this class if you want to sync data from the server to the client based on position (i.e.
+ * clients watching a chunk)
+ */
+public class PositionalDataSyncer {
+
+
+
+}
