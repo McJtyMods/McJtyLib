54d2b3820f9ca174de925e4f993170f03b5e9a81
Ray Rehbein <mrrehbein@gmail.com>
2021-12-20 07:13:15 -0600

    Bring tooltip extras migration closer to original rendering

--- src/main/java/mcjty/lib/tooltips/ClientTooltipIcon.java
+++ src/main/java/mcjty/lib/tooltips/ClientTooltipIcon.java
@@ -2,6 +2,7 @@ package mcjty.lib.tooltips;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
+import mcjty.lib.client.RenderHelper;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
@@ -43,7 +44,7 @@ public class ClientTooltipIcon implements ClientTooltipComponent, TooltipCompone
             itemRenderer.renderAndDecorateItem(item.getLeft(), x, y, p_174257_);
             // Render durability and/or cool down bar
             itemRenderer.renderGuiItemDecorations(font, item.getLeft(), x, y, "");
-            renderItemCount(font, itemRenderer, x, y, item.getLeft().getCount(), item.getRight());
+            renderItemCount(font, itemRenderer, x, y + 20, item.getLeft().getCount(), item.getRight());
             j++;
         }
     }
@@ -57,23 +58,38 @@ public class ClientTooltipIcon implements ClientTooltipComponent, TooltipCompone
             return;
         }
 
+        MultiBufferSource.BufferSource buffersource = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
+
         String s1 = count == Integer.MAX_VALUE ? "\u221E" : Integer.toString(count);
 
-        if (errorAmount >= 0) {
-            String s2 = ChatFormatting.RED + "(" + Integer.toString(errorAmount) + ")";
-            s1 = s1 + s2;
-        }
-        int width = font.width(s1);
         PoseStack pose = new PoseStack();
-        pose.translate(x + 17,y + font.lineHeight, 200D + itemRenderer.blitOffset);
-        if (width > 18) {
-            // Scale down to fit the space
-            pose.pushPose();
-            pose.scale(17F / width, 17F / width, 1F);
-            width = 17;
+        pose.translate(0, 0, 200D + itemRenderer.blitOffset);
+        drawStringToWidth(font, buffersource, pose, x, y, 18, 0xFFFFFF, s1);
+
+        if (errorAmount >= 0) {
+            String s2 = "(" + Integer.toString(errorAmount) + ")";
+            drawStringToWidth(font, buffersource, pose, x + 8, y + 17, 18, 0xFFFF0000, s2);
         }
-        MultiBufferSource.BufferSource buffersource = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
-        font.drawInBatch(s1,  -width, 0, 0xFFFFFF, true, pose.last().pose(), buffersource, false, 0, 0xFF00FF);
+
         buffersource.endBatch();
+    }
+
+    /**
+     * @param x bottom edge
+     * @param y right side
+     * @param width space to render in
+     */
+    private static void drawStringToWidth(Font font, MultiBufferSource.BufferSource buffersource, PoseStack pose, int x, int y, int width, int color, String str) {
+        pose.pushPose();
+        pose.translate(x + width, y, 0);
+        int strWidth = font.width(str);
+        if (strWidth > width) {
+            // Scale down to fit the space
+            pose.scale((float) width / strWidth, (float) width / strWidth, 1F);
+        }
+
+        font.drawInBatch(str, -strWidth, -font.lineHeight, color, true, pose.last().pose(), buffersource, false, 0, RenderHelper.MAX_BRIGHTNESS);
+
+        pose.popPose();
     }
 }
