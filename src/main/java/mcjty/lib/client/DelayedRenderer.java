1817723404479b288d75439191927deded599d96
McJty <mcjty1@gmail.com>
2022-02-23 18:15:39 +0100

    Work on the mover

--- src/main/java/mcjty/lib/client/DelayedRenderer.java
+++ src/main/java/mcjty/lib/client/DelayedRenderer.java
@@ -7,14 +7,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
 /**
  * A delayed render can be used from within a BER (TER) in order to make it possible
@@ -22,11 +21,28 @@ import java.util.function.BiConsumer;
  */
 public class DelayedRenderer {
 
+    // Renderers per render type
     private static final Map<RenderType, List<Pair<BlockPos, BiConsumer<PoseStack, VertexConsumer>>>> renders = new HashMap<>();
+
+    // Global renderers
+    private static final Map<BlockPos, BiConsumer<PoseStack, Vec3>> delayedRenders = new HashMap<>();
+    private static final Map<BlockPos, BiFunction<Level, BlockPos, Boolean>> renderValidations = new HashMap<>();
 
     public static void render(PoseStack matrixStack) {
         MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
         Vec3 projectedView = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+
+        Set<BlockPos> todelete = new HashSet<>();
+        delayedRenders.entrySet().forEach(entry -> {
+            BlockPos pos = entry.getKey();
+            BiConsumer<PoseStack, Vec3> consumer = entry.getValue();
+            if (renderValidations.getOrDefault(pos, (level, blockPos) -> false).apply(Minecraft.getInstance().level, pos)) {
+                consumer.accept(matrixStack, projectedView);
+            } else {
+                todelete.add(pos);
+            }
+        });
+
         renders.forEach((type, renderlist) -> {
             VertexConsumer consumer = buffer.getBuffer(type);
             renderlist.forEach(r -> {
@@ -43,6 +59,16 @@ public class DelayedRenderer {
         renders.clear();
 
         buffer.endBatch();
+    }
+
+    public static void addRender(BlockPos pos, BiConsumer<PoseStack, Vec3> renderer, BiFunction<Level, BlockPos, Boolean> validator) {
+        delayedRenders.put(pos, renderer);
+        renderValidations.put(pos, validator);
+    }
+
+    public static void removeRender(BlockPos pos) {
+        delayedRenders.remove(pos);
+        renderValidations.remove(pos);
     }
 
     public static void addRender(RenderType type, BlockPos pos, BiConsumer<PoseStack, VertexConsumer> renderer) {
