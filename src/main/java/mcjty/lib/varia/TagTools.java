4e9e2a7833b2a8ea1d8a1867e9fad0001df5d611
McJty <mcjty1@gmail.com>
2022-03-13 08:45:31 +0100

    More porting work. Made TagTools in McJtyLib

---
+++ src/main/java/mcjty/lib/varia/TagTools.java
@@ -0,0 +1,51 @@
+package mcjty.lib.varia;
+
+import net.minecraft.core.DefaultedRegistry;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class TagTools {
+
+    public static Iterable<Holder<Block>> getBlocksForTag(ResourceLocation rl) {
+        DefaultedRegistry<Block> registry = Registry.BLOCK;
+        return registry.getTagOrEmpty(TagKey.create(registry.key(), rl));
+    }
+
+    public static Iterable<Holder<Block>> getBlocksForTag(TagKey<Block> rl) {
+        DefaultedRegistry<Block> registry = Registry.BLOCK;
+        return registry.getTagOrEmpty(rl);
+    }
+
+    public static Iterable<Holder<Item>> getItemsForTag(ResourceLocation rl) {
+        DefaultedRegistry<Item> registry = Registry.ITEM;
+        return registry.getTagOrEmpty(TagKey.create(registry.key(), rl));
+    }
+
+    public static Iterable<Holder<Item>> getItemsForTag(TagKey<Item> rl) {
+        DefaultedRegistry<Item> registry = Registry.ITEM;
+        return registry.getTagOrEmpty(rl);
+    }
+
+    public static boolean hasTag(Block block, TagKey<Block> tag) {
+        return Registry.BLOCK.getHolderOrThrow(block.builtInRegistryHolder().key()).is(tag);
+    }
+
+    public static boolean hasTag(Item item, TagKey<Item> tag) {
+        return Registry.ITEM.getHolderOrThrow(item.builtInRegistryHolder().key()).is(tag);
+    }
+
+    public static Collection<TagKey<Item>> getTags(Item item) {
+        return item.builtInRegistryHolder().tags().collect(Collectors.toSet());
+    }
+
+    public static Collection<TagKey<Block>> getTags(Block block) {
+        return block.builtInRegistryHolder().tags().collect(Collectors.toSet());
+    }
+}
