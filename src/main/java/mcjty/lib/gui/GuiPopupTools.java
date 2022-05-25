f039106fd8db4fd33eb9f5f7d0aab3ffe5910861
McJty <mcjty1@gmail.com>
2022-05-25 09:11:10 +0200

    More work on the mover. Added a 'Move' button. Also moved some code from RFTools Control to McJtyLib

---
+++ src/main/java/mcjty/lib/gui/GuiPopupTools.java
@@ -0,0 +1,50 @@
+package mcjty.lib.gui;
+
+import mcjty.lib.gui.widgets.Panel;
+import mcjty.lib.gui.widgets.TextField;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.function.Consumer;
+
+import static mcjty.lib.gui.widgets.Widgets.*;
+
+public class GuiPopupTools {
+
+    public static void askSomething(Minecraft mc, Screen gui, WindowManager windowManager, int x, int y, String title, String initialValue, Consumer<String> callback) {
+        Panel ask = vertical()
+                .filledBackground(0xff666666, 0xffaaaaaa)
+                .filledRectThickness(1);
+        ask.bounds(x, y, 100, 60);
+        Window askWindow = windowManager.createModalWindow(ask);
+        ask.children(label(title));
+        TextField input = new TextField().addTextEnterEvent(((newText) -> {
+            windowManager.closeWindow(askWindow);
+            callback.accept(newText);
+        }));
+        input.text(initialValue);
+        ask.children(input);
+        Panel buttons = horizontal().desiredWidth(100).desiredHeight(18);
+        buttons.children(button("Ok").event((() -> {
+            windowManager.closeWindow(askWindow);
+            callback.accept(input.getText());
+        })));
+        buttons.children(button("Cancel").event((() -> {
+            windowManager.closeWindow(askWindow);
+        })));
+        ask.children(buttons);
+    }
+
+    public static void showMessage(Minecraft mc, Screen gui, WindowManager windowManager, int x, int y, String title) {
+        Panel ask = vertical()
+                .filledBackground(0xff666666, 0xffaaaaaa)
+                .filledRectThickness(1);
+        ask.bounds(x, y, 200, 40);
+        Window askWindow = windowManager.createModalWindow(ask);
+        ask.children(label(title).desiredWidth(200));
+        Panel buttons = horizontal().desiredWidth(100).desiredHeight(18);
+        buttons.children(button("Cancel").event(() -> windowManager.closeWindow(askWindow)));
+        ask.children(buttons);
+    }
+
+}
