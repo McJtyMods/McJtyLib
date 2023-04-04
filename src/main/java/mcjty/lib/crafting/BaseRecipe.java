38cac656229dc30cf7bf7a073c1bf49d75668507
McJty <mcjty1@gmail.com>
2023-04-04 06:02:12 +0200

    Change api to avoid problem with level.registryAccess() being different between 1.19.2 and 1.19.3/4

--- src/main/java/mcjty/lib/crafting/BaseRecipe.java
+++ src/main/java/mcjty/lib/crafting/BaseRecipe.java
@@ -5,15 +5,16 @@ import net.minecraft.world.Container;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.level.Level;
 import org.checkerframework.checker.units.qual.C;
 
 public interface BaseRecipe<C extends Container> extends Recipe<C> {
 
-    static ItemStack assemble(Recipe recipe, CraftingContainer pContainer, RegistryAccess access) {
+    static ItemStack assemble(Recipe recipe, CraftingContainer pContainer, Level level) {
         return recipe.assemble(pContainer);
     }
 
-    static ItemStack getResultItem(Recipe recipe, RegistryAccess access) {
+    static ItemStack getResultItem(Recipe recipe, Level level) {
         return recipe.getResultItem();
     }
 
