c787b7f50bf59fa04ef1380c59736a74ea6284b8
McJty <mcjty1@gmail.com>
2023-04-25 14:02:43 +0200

    Enabled Patchouli compatibility in 1.19.3

--- src/main/java/mcjty/lib/compat/patchouli/PatchouliCompatibility.java
+++ src/main/java/mcjty/lib/compat/patchouli/PatchouliCompatibility.java
@@ -5,14 +5,14 @@ import mcjty.lib.varia.ComponentFactory;
 import net.minecraft.ChatFormatting;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import vazkii.patchouli.api.PatchouliAPI;
 //import vazkii.patchouli.api.PatchouliAPI;
 
 public class PatchouliCompatibility {
 
     public static void openBookGUI(ServerPlayer player, ResourceLocation id) {
         if (ModSetup.patchouli) {
-            // @todo 1.19.3
-//            PatchouliAPI.get().openBookGUI(player, id);
+            PatchouliAPI.get().openBookGUI(player, id);
         } else {
             player.sendSystemMessage(ComponentFactory.literal(ChatFormatting.RED + "Patchouli is missing! No manual present"));
         }
@@ -20,8 +20,7 @@ public class PatchouliCompatibility {
 
     public static void openBookEntry(ServerPlayer player, ResourceLocation id, ResourceLocation entry, int page) {
         if (ModSetup.patchouli) {
-            // @todo 1.19.3
-//            PatchouliAPI.get().openBookEntry(player, id, entry, page);
+            PatchouliAPI.get().openBookEntry(player, id, entry, page);
         } else {
             player.sendSystemMessage(ComponentFactory.literal(ChatFormatting.RED + "Patchouli is missing! No manual present"));
         }
