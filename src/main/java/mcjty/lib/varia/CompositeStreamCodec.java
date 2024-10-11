b2c1e490c706c39b40dd173fd20acdba329c2ea7
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2024-10-11 20:01:19 +0200

    More porting of xNet

--- src/main/java/mcjty/lib/varia/CompositeStreamCodec.java
+++ src/main/java/mcjty/lib/varia/CompositeStreamCodec.java
@@ -1,5 +1,7 @@
 package mcjty.lib.varia;
 
+import com.mojang.datafixers.util.Function13;
+import com.mojang.datafixers.util.Function7;
 import com.mojang.datafixers.util.Function8;
 import com.mojang.datafixers.util.Function9;
 import net.minecraft.network.codec.StreamCodec;
@@ -7,6 +9,72 @@ import net.minecraft.network.codec.StreamCodec;
 import java.util.function.Function;
 
 public class CompositeStreamCodec {
+
+    public static <B, C, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> StreamCodec<B, C> composite(
+            final StreamCodec<? super B, T1> codec1,
+            final Function<C, T1> getter1,
+            final StreamCodec<? super B, T2> codec2,
+            final Function<C, T2> getter2,
+            final StreamCodec<? super B, T3> codec3,
+            final Function<C, T3> getter3,
+            final StreamCodec<? super B, T4> codec4,
+            final Function<C, T4> getter4,
+            final StreamCodec<? super B, T5> codec5,
+            final Function<C, T5> getter5,
+            final StreamCodec<? super B, T6> codec6,
+            final Function<C, T6> getter6,
+            final StreamCodec<? super B, T7> codec7,
+            final Function<C, T7> getter7,
+            final StreamCodec<? super B, T8> codec8,
+            final Function<C, T8> getter8,
+            final StreamCodec<? super B, T9> codec9,
+            final Function<C, T9> getter9,
+            final StreamCodec<? super B, T10> codec10,
+            final Function<C, T10> getter10,
+            final StreamCodec<? super B, T11> codec11,
+            final Function<C, T11> getter11,
+            final StreamCodec<? super B, T12> codec12,
+            final Function<C, T12> getter12,
+            final StreamCodec<? super B, T13> codec13,
+            final Function<C, T13> getter13,
+            final Function13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, C> factory) {
+        return new StreamCodec<>() {
+            @Override
+            public C decode(B buf) {
+                T1 t1 = codec1.decode(buf);
+                T2 t2 = codec2.decode(buf);
+                T3 t3 = codec3.decode(buf);
+                T4 t4 = codec4.decode(buf);
+                T5 t5 = codec5.decode(buf);
+                T6 t6 = codec6.decode(buf);
+                T7 t7 = codec7.decode(buf);
+                T8 t8 = codec8.decode(buf);
+                T9 t9 = codec9.decode(buf);
+                T10 t10 = codec10.decode(buf);
+                T11 t11 = codec11.decode(buf);
+                T12 t12 = codec12.decode(buf);
+                T13 t13 = codec13.decode(buf);
+                return factory.apply(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13);
+            }
+
+            @Override
+            public void encode(B o, C c) {
+                codec1.encode(o, getter1.apply(c));
+                codec2.encode(o, getter2.apply(c));
+                codec3.encode(o, getter3.apply(c));
+                codec4.encode(o, getter4.apply(c));
+                codec5.encode(o, getter5.apply(c));
+                codec6.encode(o, getter6.apply(c));
+                codec7.encode(o, getter7.apply(c));
+                codec8.encode(o, getter8.apply(c));
+                codec9.encode(o, getter9.apply(c));
+                codec10.encode(o, getter10.apply(c));
+                codec11.encode(o, getter11.apply(c));
+                codec12.encode(o, getter12.apply(c));
+                codec13.encode(o, getter13.apply(c));
+            }
+        };
+    }
 
     public static <B, C, T1, T2, T3, T4, T5, T6, T7, T8, T9> StreamCodec<B, C> composite(
             final StreamCodec<? super B, T1> codec1,
@@ -100,6 +168,48 @@ public class CompositeStreamCodec {
                 codec6.encode(buf, getter6.apply(obj));
                 codec7.encode(buf, getter7.apply(obj));
                 codec8.encode(buf, getter8.apply(obj));
+            }
+        };
+    }
+
+    public static <B, C, T1, T2, T3, T4, T5, T6, T7> StreamCodec<B, C> composite(
+            final StreamCodec<? super B, T1> codec1,
+            final Function<C, T1> getter1,
+            final StreamCodec<? super B, T2> codec2,
+            final Function<C, T2> getter2,
+            final StreamCodec<? super B, T3> codec3,
+            final Function<C, T3> getter3,
+            final StreamCodec<? super B, T4> codec4,
+            final Function<C, T4> getter4,
+            final StreamCodec<? super B, T5> codec5,
+            final Function<C, T5> getter5,
+            final StreamCodec<? super B, T6> codec6,
+            final Function<C, T6> getter6,
+            final StreamCodec<? super B, T7> codec7,
+            final Function<C, T7> getter7,
+            final Function7<T1, T2, T3, T4, T5, T6, T7, C> factory) {
+        return new StreamCodec<>() {
+            @Override
+            public C decode(B buf) {
+                T1 t1 = codec1.decode(buf);
+                T2 t2 = codec2.decode(buf);
+                T3 t3 = codec3.decode(buf);
+                T4 t4 = codec4.decode(buf);
+                T5 t5 = codec5.decode(buf);
+                T6 t6 = codec6.decode(buf);
+                T7 t7 = codec7.decode(buf);
+                return factory.apply(t1, t2, t3, t4, t5, t6, t7);
+            }
+
+            @Override
+            public void encode(B buf, C obj) {
+                codec1.encode(buf, getter1.apply(obj));
+                codec2.encode(buf, getter2.apply(obj));
+                codec3.encode(buf, getter3.apply(obj));
+                codec4.encode(buf, getter4.apply(obj));
+                codec5.encode(buf, getter5.apply(obj));
+                codec6.encode(buf, getter6.apply(obj));
+                codec7.encode(buf, getter7.apply(obj));
             }
         };
     }
